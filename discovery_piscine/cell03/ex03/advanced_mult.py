0b0d99d038954a00f0028de92ada718b10a0a71b
Phone Min Khant <phoneminkhant1030501@gmail.com>
2025-05-06 18:20:16 +0700

    Update advanced_mult.py

--- discovery_piscine/cell03/ex03/advanced_mult.py
+++ discovery_piscine/cell03/ex03/advanced_mult.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
+for i in range(0, 11):
   print("Table for ", i, ":", end=" ")
-  for j in range(1, 10):
+  for j in range(1, 11):
     print("{}".format(i * j), end=" ")
   print()
