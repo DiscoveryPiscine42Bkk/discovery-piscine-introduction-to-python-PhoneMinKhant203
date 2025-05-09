58397ffb0e34227809dd175a3af9fae8f52fc047
Phone Min Khant <phoneminkhant1030501@gmail.com>
2025-05-09 13:08:36 +0700

    Update chess_game.py

--- discovery_piscine/rush00/ex01/chess_game.py
+++ discovery_piscine/rush00/ex01/chess_game.py
@@ -6,20 +6,36 @@ def king_pos_generator():
     return random.randint(0,15)
 
 def main():
-    B_row = int(input("Decide your B:Bishop row position from 0 to 15 ~ "))
-    B_col = int(input("Decide your B:Bishop column position from 0 to 15 ~ "))
+    B_row = B_col = R_col = R_row = Q_row = Q_col = P_row = P_col = H_row = H_col = -1
 
-    R_row = int(input("Decide your R:Rook row position from 0 to 15 ~ "))
-    R_col = int(input("Decide your R:Rook column position from 0 to 15 ~ "))
+    while 0 > B_row or B_row > 15: 
+        B_row = int(input("Decide your B:Bishop row position from 0 to 15 ~ "))
+    while 0 > B_col or B_col > 15:
+        B_col = int(input("Decide your B:Bishop column position from 0 to 15 ~ "))
 
-    Q_row = int(input("Decide your Q:Queen row position from 0 to 15 ~ "))
-    Q_col = int(input("Decide your Q:Queen column position from 0 to 15 ~ "))
+    
+    while (0 > R_row or R_row > 15):
+        R_row = int(input("Decide your R:Rook row position from 0 to 15 ~ "))
+    while (0 > R_col or R_col > 15):
+        R_col = int(input("Decide your R:Rook column position from 0 to 15 ~ "))
+        
 
-    P_row = int(input("Decide your P:Pawn row position from 0 to 15 ~ "))
-    P_col = int(input("Decide your P:Pawn column position from 0 to 15 ~ "))
+    while (0 > Q_row or Q_row > 15):
+        Q_row = int(input("Decide your Q:Queen row position from 0 to 15 ~ "))
+    while (0 > Q_col or Q_col > 15):
+        Q_col = int(input("Decide your Q:Queen column position from 0 to 15 ~ "))
 
-    H_row = int(input("Decide your H:Knight row position from 0 to 15 ~ "))
-    H_col = int(input("Decide your H:Knight column position from 0 to 15 ~ "))
+
+    while (0 > P_row or P_row > 15):
+        P_row = int(input("Decide your P:Pawn row position from 0 to 15 ~ "))
+    while (0 > P_col or P_col > 15):
+        P_col = int(input("Decide your P:Pawn column position from 0 to 15 ~ "))
+
+
+    while (0 > H_row or H_row > 15):
+        H_row = int(input("Decide your H:Knight row position from 0 to 15 ~ "))
+    while (0 > H_col or H_col > 15):
+        H_col = int(input("Decide your H:Knight column position from 0 to 15 ~ "))
 
     chess_board = """\
 ................
